9cc32825a6eca2e1e8d9b9db2f097af9c8822e19
Atila Neves <atila.neves@gmail.com>
2015-09-12 00:11:20 +0200

    Real failing leaf tests

--- test/test.lua
+++ test/test.lua
@@ -4,8 +4,23 @@ lu = require('luaunit')
 TestJson = {}
 function TestJson:testLeafFoo()
    local foo = reggae.Target('foo.d')
-   lu.assertEquals(foo:to_json(), "{}")
+   lu.assertEquals(foo:to_json(),
+                   [[{"type": "fixed",
+                      "command": {},
+                      "outputs": ["foo.d"],
+                      "dependencies": {"type": "fixed", "targets": []},
+                      "implicits": {"type": "fixed", "targets": []}}]])
 end
+
+-- function TestJson:testLeafBar()
+--    local bar = reggae.Target('bar.d')
+--    lu.assertEquals(bar:to_json(),
+--                    [[{"type": "fixed",
+--                       "command": {},
+--                       "outputs": ["bar.d"],
+--                       "dependencies": {"type": "fixed", "targets": []},
+--                       "implicits": {"type": "fixed", "targets": []}}]])
+-- end
 
 lu.LuaUnit.verbosity = 2
 os.exit(lu.LuaUnit.run())
